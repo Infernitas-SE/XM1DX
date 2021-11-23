f1e95258d1a7cad09e3839b9618c8eaeded76d3b
Oliver Karger <kmaster@oliver-karger.de>
2021-11-23 11:20:27 +0100

    Initial Commit
    Signed-off-by: Oliver Karger <kmaster@oliver-karger.de>

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { Observable, Subject, BehaviorSubject } from 'rxjs'
+
+export class XM1DXEngine {
+		/*
+		 * Data Storage Section
+		 * All Data is stored inside RxJS' BehaviorSubject 
+		 */
+
+
+		constructor() {}
+}
